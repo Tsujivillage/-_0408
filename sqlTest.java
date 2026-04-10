4e6c9266b19b815bd68cb7d81d0c0a795f06e7c9
Tsujivillage <iga032mx@gmail.com>
2026-04-10 17:24:57 +0900

    SQL接続実験

--- sqlTest.java
+++ sqlTest.java
@@ -2,15 +2,22 @@ import java.sql.*;
 
 public class sqlTest {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://127.0.0.1:3306/mysql?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Tokyo";
+        String url = "jdbc:mysql://127.0.0.1:3306/ems2604";
         String user = "root";
         String password = "my-secret-pw";
 
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            System.out.println("接続成功: " + conn);
+        String sql = "SELECT * FROM practice";
+
+        try (
+            Connection conn = DriverManager.getConnection(url, user, password);
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery()
+        ) {
+            if (rs.next()) {
+                System.out.println("SQL実行成功: " + rs.getInt(1));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
